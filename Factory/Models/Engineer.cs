1811a7ade2a52da0ce87db717a44a506d6c100dc
chrisdfghjkl <cdepastene@gmail.com>
2022-03-24 14:35:08 -0700

    add model business logic

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,5 +4,13 @@ namespace Factory.Models
 {
   public class Engineer
   {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
   }
 }
